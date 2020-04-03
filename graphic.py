f310905e048074643d68e8964213fa08a1df416e
jptrzy <41678464+jptrzy@users.noreply.github.com>
2020-04-03 10:52:37 +0200

    Color Terminal

--- graphic.py
+++ graphic.py
@@ -1,7 +1,45 @@
+from tkinter import *
+import tkinter.font as TkFont
+import os
+import time
+
+
+
+root = Tk()
+root.title("Szkielet Boga")
+root.geometry("500x200")
+
+dFont=TkFont.Font(family="Arial", size=12)
+#dFont=TkFont.Font(family="Verdana", size=12)
+
+f = Frame()
+f.pack(side=TOP, fill=BOTH, expand = 1)
+
+text1 = Text(f, height=1, width=50, font=dFont, background="#000000", foreground="#00ff00", state="disabled", cursor="arrow")
+text1.pack(side=LEFT, fill=BOTH, expand = 1)
+
+yscrollbar=Scrollbar(f, orient=VERTICAL, command=text1.yview, bg="black", activebackground="#313131")
+yscrollbar.pack(side=LEFT, fill=Y, expand = 0)
+text1["yscrollcommand"]=yscrollbar.set
+
+v = StringVar()
+entry = Entry(root, textvariable=v, font=dFont, background="#000000", foreground="#00ff00")
+entry.pack(side=TOP ,fill=X, expand = 0)
+
 
 def say(x):
-    print(x)
-    return(0)
+    text1.config(state="normal")
+    text1.insert(INSERT,  "\n" + str(x))
+    text1.config(state="disabled")
 
-def inp():
-    return(input())
+    text1.see(END)
+    text1.edit_modified(0)
+
+def send(event=None):
+    say(v.get())
+    v.set("")
+
+
+def runGui(send):
+    root.bind('<Return>', send)
+    root.mainloop()
--- run.py
+++ run.py
@@ -1,79 +1,108 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-
-import tkinter
-
 import random as R
+import color_console as cons
 import json as J
 import os
 import time
 import codecs
+import sys
+
+default_colors = cons.get_text_attr()
+default_bg = default_colors & 0x0070
+default_fg = default_colors & 0x0007
+
+def inp():
+    return(input())
+
+def say(x):
+    cons.set_text_attr(cons.FOREGROUND_GREEN | default_bg |
+                     cons.FOREGROUND_INTENSITY)
+    print(x)
+    cons.set_text_attr(default_colors)
 
 run = 1
 
-commands = {"say":"sas"}
+day = 0
 
-#LOADING JSONS
+global dialogueJson
+dialogueJson = {}
+
+global des
+global dialogues
+des = ""
+dialogues = []
+
+##Loads
+
+def loadDialoguesJson():
+    global dialogueJson
+    dialogueJson = {}
+    for x in os.listdir("data/dialogues/"):
+        print(x)
+        dialogueJson.update( J.load(codecs.open('data/dialogues/'+str(x), 'r', 'utf-8-sig')) )
+    print(dialogueJson)
+
+
+def loadRoad():
+    global des
+    global dialogues
+    data = J.load(codecs.open('data/road/'+str(day)+'.json', 'r', 'utf-8-sig'))
+    des = data["des"]
+    print(des)
+    dialogues = data["dialogues"]
+
+##Commands
+
+def lookAroudn(args):
+    say(des)
+    return(0)
+
+def dialogue(id):
+    data = dialogueJson
+
+
+
+
+
+    return(0)
+
+def cDialogue(args):
+    return(0)
+
+commands ={
+"rozejrzyj":lookAroudn,
+"rozmawiaj":cDialogue
+}
+
+##LOADING JSONS
 
 data = J.load(codecs.open('data/errorCommands.json', 'r', 'utf-8-sig'))
-errorCommandsLenght = data["lenght"]
 errorCommandsList = data["texts"]
 
 
 
-from tkinter import *
-import tkinter.font as TkFont
 
-root = Tk()
-root.title("Szkielet Boga")
-root.geometry("500x200")
+dialogue(1001)
 
-dFont=TkFont.Font(family="Arial", size=12)
-#dFont=TkFont.Font(family="Verdana", size=12)
+loadDialoguesJson()
 
-f = Frame()
-f.pack(side=TOP, fill=BOTH, expand = 1)
+loadRoad()
 
-text1 = Text(f, height=1, width=50, font=dFont, background="#000000", foreground="#00ff00", state="disabled", cursor="arrow")
-text1.pack(side=LEFT, fill=BOTH, expand = 1)
+while(run):
+    ##Road Segment
 
-yscrollbar=Scrollbar(f, orient=VERTICAL, command=text1.yview, bg="black", activebackground="#313131")
-yscrollbar.pack(side=LEFT, fill=Y, expand = 0)
-text1["yscrollcommand"]=yscrollbar.set
-
-v = StringVar()
-entry = Entry(root, textvariable=v, font=dFont, background="#000000", foreground="#00ff00")
-entry.pack(side=TOP ,fill=X, expand = 0)
-
-
-def say(x):
-    text1.config(state="normal")
-    text1.insert(INSERT,  "\n" + str(x))
-    text1.config(state="disabled")
-
-    text1.see(END)
-    text1.edit_modified(0)
-
-def loop():
-
-    #say("{}".format(time.time()))
-
-    root.after(2000, loop)  # reschedule event in 2 seconds
-
-
-def send(event=None):
-    co = v.get()
+    co = inp()
     col = co.split()
 
     if(col[0] in commands):
-        say(commands[col[0]])
-        say(col)
+        print(0)
+        commands[col[0]](col)
     else:
         say(R.choice(errorCommandsList).format(co))
 
-    v.set("")
+    ##Rest Segment
 
-root.bind('<Return>', send)
+    ##Dream Segment
 
-root.after(2000, loop)
-root.mainloop()
+    ##Dawn Segment
